08a9d51509806b589130e0d564df209a7bb6de01
Fredrik Svantes <fredrik@ethereum.org>
2025-01-16 11:46:58 +0100

    Add blog links

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -42,10 +42,45 @@ interface Props {
 // add here the list of external blog posts, with title, date and link
 const externalLinks = [
   {
-    title: 'Writing Robust C - Best Practices for Finding and Preventing Vulnerabilities',
+    title: 'ETH Rangers Program',
+    date: '2024-12-02',
+    link: 'https://blog.ethereum.org/2024/12/02/ethrangers-public-goods'
+  },
+  {
+    title: 'Ethereum Protocol Attackathon Announcement',
+    date: '2024-11-25',
+    link: 'https://blog.ethereum.org/2024/11/25/ethereum-protocol-attackathon'
+  },
+  {
+    title: 'Secured #6 - Writing Robust C - Best Practices for Finding and Preventing Vulnerabilities',
     date: '2023-11-02',
     link: 'https://blog.ethereum.org/2023/11/02/writing-robust-c'
-  }
+  },
+  {
+    title: 'Secured #5 - Public Vulnerability Disclosures',
+    date: '2023-05-03',
+    link: 'https://blog.ethereum.org/2023/05/03/secured-5-disclosures-update'
+  },
+  {
+    title: 'Secured #4 - Bug Bounty Rewards to $250,000',
+    date: '2022-05-16',
+    link: 'https://blog.ethereum.org/2022/05/16/secured-no-4'
+  },
+  {
+    title: 'Secured #3 - Security Teams',
+    date: '2022-04-22',
+    link: 'https://blog.ethereum.org/2022/04/14/secured-no-3'
+  },
+  {
+    title: 'Secured #2 - Public Vulnerability Disclosures',
+    date: '2022-03-09',
+    link: 'https://blog.ethereum.org/2022/03/09/secured-no-2'
+  },
+  {
+    title: 'Secured #1 - BLS is Everywhere',
+    date: '2021-09-09',
+    link: 'https://blog.ethereum.org/2021/09/09/secured-no-1'
+  },  
 ];
 
 const Blog: NextPage<Props> = ({ posts }) => {
